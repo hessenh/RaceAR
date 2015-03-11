ad17a72397f69852e8b049d426fd2c9041b27598
Hans-Olav <hansolav_91@hotmail.com>
2015-03-11 10:32:14 -0700

    Cont. tutorial

--- app/src/main/java/com/qualcomm/vuforia/samples/VuforiaSamples/app/ImageTargets/Tutorial.java
+++ app/src/main/java/com/qualcomm/vuforia/samples/VuforiaSamples/app/ImageTargets/Tutorial.java
@@ -26,6 +26,8 @@ public class Tutorial extends Activity implements Button.OnClickListener {
         mTutorialImg = (ImageView)findViewById(R.id.imageView);
         mTutorialImg.setImageResource(R.drawable.menueraw);
 
+        counter = 0;
+
         text = (TextView)findViewById(R.id.textView);
         text.setText("In VirTrack you can either create your own track or join an exciting one!");
 
@@ -39,34 +41,60 @@ public class Tutorial extends Activity implements Button.OnClickListener {
     }
     @Override
     public void onClick(View view) {
+        switch(view.getId()) {
+            case R.id.nextBtn:
+                counter++;
+                break;
+            case R.id.prevBtn:
+                counter--;
+                break;
+        }
+        System.out.println(counter);
+
         switch (counter) {
             case 0:
+                mTutorialImg.setImageResource(R.drawable.menueraw);
+                text.setText("In VirTrack you can either create your own track or join an exciting one!");
+                break;
+            case 1:
                 mTutorialImg.setImageResource(R.drawable.markerraw);
                 text.setText("The blue marker shows where the part will attach.\n " +
                         "Hold your finger on the screen to get the a new part!");
                 break;
-            case 1:
+            case 2:
                 mTutorialImg.setImageResource(R.drawable.part);
                 text.setText("This is a straight part! You can move your phone to place it where you like!");
                 break;
-            case 2:
+            case 3:
                 mTutorialImg.setImageResource(R.drawable.rotateraw);
                 text.setText("Tap once to rotate the part 90 degrees");
                 break;
-            case 3:
+            case 4:
                 mTutorialImg.setImageResource(R.drawable.placepartraw);
                 text.setText("Hold your finger on the screen to place the part");
                 break;
-        }
-        if(view.getId()==R.id.nextBtn){
-
-            counter++;
-        }
-        else if(view.getId()==R.id.prevBtn){
-            counter--;
-        }
-        if(counter<0){
-            counter=0;
+            case 5:
+                mTutorialImg.setImageResource(R.drawable.placepartraw);
+                text.setText("Swipe your finger from left to right to get to the menu");
+                break;
+            case 6:
+                mTutorialImg.setImageResource(R.drawable.finished);
+                text.setText("When the track is finished, a blue box will try it out!");
+                break;
+            case 7:
+                mTutorialImg.setImageResource(R.drawable.duellblur);
+                text.setText("Now it's time to test out the track! \n " +
+                        "There are two ways of doing that: \n" +
+                        "\n" +
+                        "- Create a game and play \n" +
+                        "- Go directly to lobby");
+                break;
+            case 8:
+                mTutorialImg.setImageResource(R.drawable.duellblur);
+                text.setText("If you're the creator of the track, insert your opponents IP and connect, send track and play! \n" +
+                        "\n" +
+                        "If not, wait for the connection and track, then play!");
+                break;
         }
     }
 
